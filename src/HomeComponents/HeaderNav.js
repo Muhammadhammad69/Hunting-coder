4a022fbfb8ccfebd594615561b9c53d38cbabe97
fareeha03 <hammadak69@gmail.com>
2024-08-28 12:17:21 +0500

    u

--- src/HomeComponents/HeaderNav.js
+++ src/HomeComponents/HeaderNav.js
@@ -106,7 +106,7 @@ const HeaderNav = ({ pathName }) => {
               <li className={navStyle.effect}>
                 <Link
                   href="/blog"
-                  className=" block py-2 px-3 text-gray-950 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:bg-transparent "
+                  className=" block py-2 px-3 text-gray-950 rounded  md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500  dark:hover:text-white md:dark:hover:bg-transparent md:bg-transparent "
                   onClick={clickFunction}
                 >
                   Blog
